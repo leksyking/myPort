a2f6cf5c8bf7a4830435a7dc6bb8fec74f95cbf5
Leksyking <gbemilekeogundipe@gmail.com>
2022-03-16 18:44:19 -0700

    handle mails with ethereal

---
+++ controller/sendMail.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer") 
+
+const sendMail = () => {
+const output = 
+`
+<p>You have a new contact request </p>
+<h3> Contact details </h3>
+<ul>
+    <li>Name: ${req.body.fname}</li>
+    <li>Email: ${req.body.email}</li>
+</ul>
+<h3>Message</h3>
+<p>${req.body.message}</p>
+`
+let transport = nodemailer.createTransport({
+    host: process.env.SMIP_HOST,
+    port: 2525,
+    auth: {
+      user: process.env.SMIP_USER,
+      pass: process.env.SMIP_PASSWORD
+    }
+  });
+
+let mailOptions = {
+from: '"Nodemailer Contact" <process.env.EMAIL>',
+to: req.body.email,
+subject: req.body.subject,
+text: 'Hey there, it’s our first message sent with Nodemailer ',
+html: output,
+};
+
+transport.sendMail(mailOptions, (error, info) => {
+if (error) {
+return console.log(error);
+}
+console.log('Message sent: %s', info.messageId);
+}); 
+}
+
+module.exports = sendMail;
